4103ccab0bbe5c04e3c73f4ff8bedb09183407b7
NatCardonaRi <natalia.cardona9@udea.edu.co>
2022-12-29 09:34:07 -0500

    Sprint 2 finalizado

---
+++ main.js
@@ -0,0 +1,5 @@
+//Capture DOM'S elements
+const btnSubmit = document.getElementById('submit');
+const segundoContainer = document.getElementById('container-thanks');
+const primerContainer = document.getElementById('all');
+
